3077426d9dfe189ae75594eeb5483cca4fdcd35c
Prateek kumar <pbprateek2@gmail.com>
2017-12-05 22:06:11 +0530

    Initial setup

---
+++ app/src/main/java/com/ATG/World/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.ATG.World;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+       //We will use shared preference to check login activity which we will add later
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+
+
+    }
+}
