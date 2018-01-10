c726a8abc67d03a7cc258d62900fb397d50368a6
jack1806 <yashtodkar2@gmail.com>
2018-01-09 21:14:39 -0500

    Group details feature work started

---
+++ app/src/main/java/com/ATG/World/activity/GroupDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.ATG.World.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_details);
+    }
+}
